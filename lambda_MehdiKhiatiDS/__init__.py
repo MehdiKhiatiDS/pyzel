9f635aa1cc3aefa03f168b1f0faf7405ff077abe
Mehdi Khiati <mehdi1710@me.com>
2020-01-15 15:51:18 -0800

    getting init

---
+++ lambda_MehdiKhiatiDS/__init__.py
@@ -0,0 +1,12 @@
+"""
+Lambda Mehdi Stuff
+"""
+
+import pandas as pd 
+import numpy as np 
+from sklearn.linear_model import LinearRegression
+
+
+# sample code
+ONE =  pd.DataFrame(np.ones(10))
+Zeros = pd.DataFrame(np.zeros(50))
